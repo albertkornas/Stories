77d67259262ce34b73019babc48aed3647968491
Albert Kornas <albertkornas@gmail.com>
2021-08-10 17:40:27 -0400

    Initial commit

---
+++ Stories/StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesApp.swift
+//  Stories
+//
+//  Created by Albert Kornas on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
